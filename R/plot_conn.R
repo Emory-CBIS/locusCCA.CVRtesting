2629a3c46e0bb5c627bcf95c01da3e8ee83bb546
Gyang-eng <gyang52@emory.edu>
2025-03-04 22:16:07 -0500

    Update plot_conn.R

--- R/plot_conn.R
+++ R/plot_conn.R
@@ -17,7 +17,16 @@ plot_conn <- function(mat){
   label <- read.csv(csv_path)
   original_label <- label[order(label$Original_ROI), ]
   original_label <- tibble(Node = original_label$smithRSN, label = original_label$SmithName)
-  
+original_label$label[original_label$label=="Occ. Pole"]= 'Op Vis'
+original_label$label[original_label$label=="Unknown"]= 'Other'
+original_label$label[original_label$label=="Executive Control"]= 'EC'
+original_label$label[original_label$label=="Auditory"]= 'Aud'
+original_label$label[original_label$label=="Default Mode"]= 'DMN'
+original_label$label[original_label$label=="Cerebellum"]= 'CB'
+#original_label$label[original_label$label=="MR"]= 'DMN'
+original_label$label[original_label$label=="Unknown"]= 'Other'
+original_label$label[original_label$label=="Subcortical"]= 'SC'
+original_label$label[original_label$label%in%c("DMN R","DMN P","DMN F","DMN L")]= 'DMN'
   # Generate plot
  plot_network_heatmap1(mat / max(abs(mat)), 
                         network_information = original_label, 
